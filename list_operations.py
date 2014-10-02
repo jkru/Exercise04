fd81db42880536637734f2d73b510d747db96de3
Hackbright Student <info@hackbrightacademy.com>
2014-10-02 16:51:31 -0700

    first half completed, code review by Lindsay

--- list_operations.py
+++ list_operations.py
@@ -24,64 +24,73 @@ def head(input_list):
 
 def tail(input_list):
     """Return all elements of the input list except the first."""
-    pass
+    return input_list[1:]
 
 def last(input_list):
     """Return the last element of the input list."""
-    pass
+    return input_list[-1]
 
 def init(input_list):
     """Return all elements of the input list except the last."""
-    pass
+    return input_list[:-1]
 
 def first_three(input_list):
     """Return the first three elements of the input list."""
-    pass
+    return input_list[0:3]
 
 def last_five(input_list):
     """Return the last five elements of the input list."""
-    pass
+    return input_list[-5:]
 
 def middle(input_list):
     """Return all elements of the input list except the first two and the last
     two.
     """
-    pass
+    return input_list[2:-2]
 
 def inner_four(input_list):
     """Return the third, fourth, fifth, and sixth elements of the input list."""
-    pass
+    return input_list[2:6]
 
 def inner_four_end(input_list):
     """Return the sixth, fifth, fourth, and third elements from the end of the
     list, in that order.
     """
-    pass
+    return input_list[-6:-2]
 
 def replace_head(input_list):
     """Replace the head of the input list with the value 42."""
-    pass
+    input_list[0]=42
+    return input_list
 
 def replace_third_and_last(input_list):
     """Replace the third and last elements of the input list with the value 37."""
-    pass
+    input_list[2],input_list[-1] = 37,37
+    #input_list[2] = 37
+    #input_list[-1] = 37
+    return input_list
 
 def replace_middle(input_list):
     """Replace all elements of the input list with the the values 42 and 37, in
     that order, except for the first two and last two elements.
     """
-    pass
-
+    input_list[2:-2] = [42, 37]
+    return input_list
+    
 def delete_third_and_seventh(input_list):
     """Remove the third and seventh elements of the input list."""
+    del input_list[2]
+    del input_list[5]
+    return input_list
     pass
 
 def delete_middle(input_list):
     """Remove all elements from the input list except for the first two and the
     last two.
     """
+    input_list[2:-2] = []
+    return input_list
     pass
-
 """
 Now would be a great time to ask for a code review.
 """
@@ -104,7 +113,10 @@ the test_list_operations.py file for concrete examples of expected behavior.
 
 def custom_len(input_list):
     """custom_len(input_list) imitates len(input_list)"""
-    pass
+    listlength = 0
+    for i in input_list:
+        listlength = listlength + 1
+    return listlength
 
 # For the next four functions, get clever using slice operations described in the first half
 def custom_append(input_list, value):
